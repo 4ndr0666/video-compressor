8f4e167117b5db87bb8753891985736eecab5f68
4ndr0666 <01_dolor.loftier@icloud.com>
2025-07-06 19:25:13 -0500

    updated entrypoints.sh

--- setup.py
+++ setup.py
@@ -12,5 +12,9 @@ setup(
         "PyQt6>=6.6.1",
         "requests>=2.31.0",
     ],
-    entry_points={"console_scripts": ["videocompressor=main:main"]},
+    entry_points={
+        "console_scripts": [
+            "videocompressor=main:main"
+        ]
+    },
 )
